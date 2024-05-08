8abbb8e0db71ce1fed46955b31e5b34e33c25417
Jozua Sijsling <jozua.sijsling@ing.com>
2024-05-08 21:18:48 +0200

    Fix build.sh script and simplify it by removing environment variable checking, relying instead on Gradle wrapper and AGP for detecting and reporting such issues.

--- build.sh
+++ build.sh
@@ -1,17 +1,8 @@
+#!/usr/bin/env bash
 ### Bash script for building Talkback-for-Partners Android apk
-###
-### The following environment variables must be set before executing this script
-###   ANDROID_SDK           # path to local copy of Android SDK
-###   ANDROID_NDK           # path to local copy of Android NDK
-###   JAVA_HOME             # path to local copy of Java SDK.
-
-# For help in getting the correct version numbers of gradle, the gradle plugin,
-# and Java, see the following:
-# https://developer.android.com/build/releases/gradle-plugin#updating-gradle
-# https://docs.gradle.org/current/userguide/compatibility.html
-GRADLE_DOWNLOAD_VERSION=7.3.3
+WORK_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 GRADLE_TRACE=false   # change to true to enable verbose logging of gradle
-
+GRADLE_WRAPPER="$WORK_DIR/gradlew"
 
 function log {
   if [[ -n $1 ]]; then
@@ -20,79 +11,13 @@ function log {
   fi
 }
 
-function fail_with_message  {
-  echo
-  echo "Error: ${1}"
-  exit 1
-}
-
-function require_environment_variable() {
-  if [[ -z ${!1+set} ]]; then
-    fail_with_message "the environment variable $1 is not set"
-    exit 1
-  else
-    log "${1}: ${!1}"
-  fi
-}
-
-function require_folder_exists() {
-  if [[ ! -d "${1}" ]]; then
-    fail_with_message "the folder at ${1} does not exist"
-    exit 1
-  else
-    log "ls ${1}"; ls "${1}"
-  fi
-}
-
-
 log "pwd: $(pwd)"
 log "ls"; ls
 log
 
-
-require_environment_variable ANDROID_SDK
-require_folder_exists "${ANDROID_SDK}"
-require_environment_variable ANDROID_NDK
-require_folder_exists "${ANDROID_NDK}"
-require_environment_variable JAVA_HOME
-require_folder_exists "${JAVA_HOME}"
-log
-
-
-log "Write local.properties file"
-echo "sdk.dir=${ANDROID_SDK}" > local.properties
-echo "ndk.dir=${ANDROID_NDK}" >> local.properties
-log "cat local.properties"; cat local.properties
-log
-
-
-# Download the gradle binary at the version set atop this file
-GRADLE_ZIP_REMOTE_FILE=gradle-${GRADLE_DOWNLOAD_VERSION}-bin.zip
-GRADLE_ZIP_DEST_PATH=~/${GRADLE_DOWNLOAD_VERSION}.zip
-log "Download gradle binary from the web ${GRADLE_ZIP_REMOTE_FILE} to ${GRADLE_ZIP_DEST_PATH} using wget"
-time wget -O ${GRADLE_ZIP_DEST_PATH} https://services.gradle.org/distributions/${GRADLE_ZIP_REMOTE_FILE}
-log
-
-
-# Unzip the gradle binary
-GRADLE_UNZIP_HOSTING_FOLDER=/opt/gradle-${GRADLE_DOWNLOAD_VERSION}
-log "Unzip gradle zipfile ${GRADLE_ZIP_DEST_PATH} to ${GRADLE_UNZIP_HOSTING_FOLDER}"
-sudo unzip -n -d ${GRADLE_UNZIP_HOSTING_FOLDER} ${GRADLE_ZIP_DEST_PATH}
-GRADLE_BINARY=${GRADLE_UNZIP_HOSTING_FOLDER}/gradle-${GRADLE_DOWNLOAD_VERSION}/bin/gradle
-log "\${GRADLE_BINARY} = ${GRADLE_BINARY}"
-log "\${GRADLE_BINARY} -version"
-${GRADLE_BINARY} -version
-log
-
-
-log "find gradle"
-find gradle
-log
-
-
 if [[ "$GRADLE_TRACE" = true ]]; then
-  log "${GRADLE_BINARY} dependencies"
-  ${GRADLE_BINARY} dependencies
+  log "${GRADLE_WRAPPER} dependencies"
+  ${GRADLE_WRAPPER} dependencies
   log
 fi
 
@@ -103,15 +28,15 @@ if [[ "$GRADLE_TRACE" = true ]]; then
   GRADLE_DEBUG=--debug
   GRADLE_STACKTRACE=--stacktrace
 fi
-log "${GRADLE_BINARY} assembleDebug"
-${GRADLE_BINARY} ${GRADLE_DEBUG} ${GRADLE_STACKTRACE} assembleDebug
+log "${GRADLE_WRAPPER} assembleDebug"
+${GRADLE_WRAPPER} ${GRADLE_DEBUG} ${GRADLE_STACKTRACE} assembleDebug
 BUILD_EXIT_CODE=$?
 log
 
 
 if [[ $BUILD_EXIT_CODE -eq 0 ]]; then
-  log "find . -name *.apk"
-  find . -name "*.apk"
+  log "find $WORK_DIR -name *.apk"
+  find $WORK_DIR -name "*.apk"
   log
 fi
 
